824d4d7bc8d467d768cdbdcbb75de9e01f6ae4b5
Monika Kumari <your-github-monikapratima7870@gmail.com>
2026-07-08 19:10:28 +0530

    Initial Commit

--- REACT/src/App.jsx
+++ REACT/src/App.jsx
@@ -1,30 +1,16 @@
 import React from 'react'
-import logo from "./assets/LOGO.jpg";
- const Header = () =>{
-  return (
-    <div className='header'>
-      <div className='logo-container'>
-        <img className="logo" src={logo} alt="Logo"></img>
-      </div>
-      <div className='nav-items'>
-       <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact Us</li>
-        <li>Cart</li>
-       </ul>
-      </div>
-    </div>
-  );
- }
 
- const RestaurantCard = () =>{
+import Header from './Component/Header';
+
+ const RestaurantCard = (props) =>{
+  console.log(props);
   return(
     <div className="res-card">
       <img className="res-logo" src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6MFXwHTJXSpL_QuRxQ9-TgPMfn-4BI0AwAE-j54s-dg&s=10' alt="res-logo"/>
-      <h3>Coffee Shop</h3>
-      <h4>4.4 stars</h4>
-      <h4>Delivery in 10 minutes</h4>
+      <h4>{props.resName}</h4>
+      <h3>{props.cuisine}</h3>
+      <h4>{props.rating}</h4>
+      <h4>{props.Deliver}</h4>
     </div>
   )
  }
@@ -33,12 +19,9 @@ import logo from "./assets/LOGO.jpg";
     <div className="body">
       <div className="Search">Search</div>
       <div className='res-container'>
-         <RestaurantCard/>
-         <RestaurantCard/>
-         <RestaurantCard/>
-         <RestaurantCard/>
-         <RestaurantCard/>
-         <RestaurantCard/>
+         <RestaurantCard resName="Fassoss" cuisine="Dal Roti.Fast deliver" rating="2.4" Deliver="10 min delivery"/>
+         <RestaurantCard resName="KFC" cuisine="Burger.Fast deliver" rating="4.9" Deliver="10 min delivery" />
+       
          
 
   
